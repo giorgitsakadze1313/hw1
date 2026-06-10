6cbdf7ad1ae5d1849046bfb951ce36c055c3c74c
giorgitsakadze1313 <tsakadzegiorgi9@gmail.com>
2026-06-10 11:00:59 +0400

    Add project files.

---
+++ homework/Program.cs
@@ -0,0 +1,72 @@
+﻿namespace homework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /* პირველი დავალება
+            Console.WriteLine("შეიყვანეთ ასაკი:");
+
+            string ageStr = Console.ReadLine();
+
+            bool isValid = int.TryParse(ageStr, out int age);
+
+            if (isValid)
+            {
+                if (age >= 18)
+                {
+                    Console.WriteLine("ხმის მიცემის უფლება გაქვთ");
+                }
+                else
+                {
+                    Console.WriteLine("სამწუხაროდ ხმის მიცემის უფლება ჯერ არ გაქვთ");
+                    */
+
+            /* მეორე 
+            Console.WriteLine("შეიყვანეთ პირველი რიცხვი");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("შეიყვანეთ მე-2 რიცხვი");
+            int num2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("შეიყვანეთ მე-3 რიცხვი");
+            int num3 = int.Parse(Console.ReadLine());
+
+            if (num1 > num2 && num1 > num3)
+            {
+                Console.WriteLine("პირველი რიცხვი ყველაზე დიდია.");
+            }
+            else if (num2 > num1 && num2 > num3)
+            {
+                Console.WriteLine("მე-2 რიცხვი ყველაზე დიდია.");
+            }
+            else
+            {
+                Console.WriteLine("მე-3 რიცხვი ყველაზე დიდია.");
+            }
+            */
+
+            /* მესამე დავალება
+            Console.WriteLine("Enter first number");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter second number");
+            int num2 = int.Parse(Console.ReadLine());
+
+            int sum = num1 + num2;
+
+            if (num1 == num2)
+            {
+                Console.WriteLine(sum * 3);
+            }
+            else
+            {
+                Console.WriteLine(sum);
+            }
+            */
+           
+
+        }
+        }
+    }
