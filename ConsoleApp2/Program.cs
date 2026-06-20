75f08a46fee9310fe84b0a9b674a75706326df85
demetred093-eng <demetred093@gmail.com>
2026-06-20 17:22:33 +0400

    weh

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* პირველი დავალება
+            int[] arr1 = { 1, 2, 3 };
+            int[] arr2 = { 4, 5, 6 };
+
+            int[] result = new int[arr1.Length + arr2.Length];
+
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                result[i] = arr1[i];
+            }
+
+            for (int i = 0; i < arr2.Length; i++)
+            {
+                result[arr1.Length + i] = arr2[i];
+            }
+
+            foreach (int item in result)
+            {
+                Console.Write(item + " ");
+            }
+            */
+
+            /* მეორე დავალება
+            int[] nums = { 3, 5, -4, 8, 11, 1, -1, 6 };
+
+            Console.WriteLine("Enter target sum");
+
+            bool valid =
+                int.TryParse(Console.ReadLine(), out int targetSum);
+
+            if (valid)
+            {
+                for (int i = 0; i < nums.Length; i++)
+                {
+                    for (int j = i + 1; j < nums.Length; j++)
+                    {
+                        if (nums[i] + nums[j] == targetSum)
+                        {
+                            Console.WriteLine(
+                                $"[{nums[i]}, {nums[j]}]"
+                            );
+                        }
+            */
+                    }
+                }
+            }
+
+       
+    
+
